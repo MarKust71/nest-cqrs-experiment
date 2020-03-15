aad222643000a688c53e37ef894f12569d0f055a
MareK <markust71@gmail.com>
2020-03-15 17:18:45 +0100

    maybe this way...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WaterConsumptionModule } from './water-consumption/water-consumption.module';
+import { WaterConsumptionApiModule } from './water-consumption-api/water-consumption-api.module';
 
 @Module({
-  imports: [],
+  imports: [WaterConsumptionModule, WaterConsumptionApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
