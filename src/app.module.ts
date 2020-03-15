432ea83ec310246f6f1aa8926f53a48b3c9d7139
MareK <markust71@gmail.com>
2020-03-15 15:51:04 +0100

    let's save it, though it was wrong way...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WaterConsumptionController } from './water-consumption/water-consumption.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, WaterConsumptionController],
   providers: [AppService],
 })
 export class AppModule {}
